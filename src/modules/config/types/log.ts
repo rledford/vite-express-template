6f7a350c9e3598a2d8736cbacf60fc71669fdc92
rledford <ryan.c.ledford@gmail.com>
2023-07-09 20:06:42 -0500

    chore: Removes "debug" from log level schema.

--- src/modules/config/types/log.ts
+++ src/modules/config/types/log.ts
@@ -3,7 +3,6 @@ import { z } from 'zod';
 export type LogLevel = z.infer<typeof LogLevelSchema>;
 export const LogLevelSchema = z.union([
   z.literal('none'),
-  z.literal('debug'),
   z.literal('info'),
   z.literal('warn'),
   z.literal('error')
