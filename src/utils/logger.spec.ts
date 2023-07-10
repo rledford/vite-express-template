43f4479874df2970dc256cee7371778338d3ec58
rledford <ryan.c.ledford@gmail.com>
2023-07-09 21:18:52 -0500

    chore: Adds expected logger def to logger spec.

--- src/utils/logger.spec.ts
+++ src/utils/logger.spec.ts
@@ -1,24 +1,22 @@
 import { createLogger } from 'vite';
 import { noopLogger } from './logger';
 
+const expectedLogger = expect.objectContaining({
+  info: expect.any(Function),
+  warn: expect.any(Function),
+  error: expect.any(Function)
+});
+
 describe('logger', () => {
   describe('createLogger', () => {
     it('should return a logger', () => {
-      const logger = createLogger();
-
-      expect(logger.info).toEqual(expect.any(Function));
-      expect(logger.warn).toEqual(expect.any(Function));
-      expect(logger.error).toEqual(expect.any(Function));
+      expect(createLogger()).toMatchObject(expectedLogger);
     });
   });
 
   describe('noopLogger', () => {
     it('should return a no-op logger', () => {
-      const logger = noopLogger();
-
-      expect(logger.info).toEqual(expect.any(Function));
-      expect(logger.warn).toEqual(expect.any(Function));
-      expect(logger.error).toEqual(expect.any(Function));
+      expect(noopLogger()).toMatchObject(expectedLogger);
     });
   });
 });
